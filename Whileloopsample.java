4e0033aaab58d3409eb99a409b531b848b77ef20
Nithya341 <nithya12102000@gmail.com>
2024-10-19 19:43:18 +0530

    Assignment 62

---
+++ Whileloopsample.java
@@ -0,0 +1,22 @@
+package basicsprograms;
+
+public class Whileloopsample 
+{
+	public static void main(String[] args) 
+	{
+		int i=1;
+		while( i<=100)
+		{	
+			System.out.println(i);
+			i++;
+		}
+		
+		
+		
+		
+	}
+	
+	
+	
+	
+}
