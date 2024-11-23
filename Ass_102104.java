b7f4bab4fc5203871d48339d5ddb3b51b0d79628
Nithya341 <nithya12102000@gmail.com>
2024-11-23 10:29:54 +0530

    Ass102104

---
+++ Ass_102104.java
@@ -0,0 +1,30 @@
+package testngprograms;
+
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class Ass_102104 {
+	@BeforeMethod
+	public void login()
+	{
+		
+	}
+	@Test(enabled = false)
+	public void tc1()
+	{
+		
+	}
+	@Test
+	public void tcw1()
+	{
+		
+	}
+    @AfterMethod
+    
+    	public void logout() {
+    	
+    	
+    }
+  
+}
