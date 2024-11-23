7b8a367ab2b9a2cfd7be9c501807990221cc1882
Nithya341 <nithya12102000@gmail.com>
2024-11-23 09:50:22 +0530

    Ass95 96

---
+++ TestNGProgram.java
@@ -0,0 +1,17 @@
+package testngprograms;
+
+import org.testng.annotations.Test;
+
+public class TestNGProgram 
+{
+	@Test(enabled=true,priority=-1)
+	public void login()
+	{
+		
+	}
+	@Test
+	public void logout()
+	{
+		
+	}
+}
---
+++ TestNGProgram4.java
@@ -0,0 +1,11 @@
+package testngprograms;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+public class TestNGProgram4 
+{
+		@Test(timeOut=3000)
+	public void testcase6()
+	{
+		ChromeDriver driver=new 	ChromeDriver();
+	}	
+}
