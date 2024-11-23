e3b2d29331f63c1c1a3da81f9fe2f2c7310064c9
Nithya341 <nithya12102000@gmail.com>
2024-11-23 09:46:18 +0530

    Assignment 94

---
+++ TestCase1.java
@@ -0,0 +1,22 @@
+package testngprograms;
+
+import org.testng.annotations.Test;
+
+public class TestCase1 
+{
+	@Test
+	public void login()
+	{
+		
+	}
+	@Test(priority=-1)
+	public void logout()
+	{
+		
+	}
+	@Test(priority=-1)
+	public void registartion()
+	{
+		
+	}
+}
