9a4669f4ccc048aa6150c711614fa98b78f45660
Nithya341 <nithya12102000@gmail.com>
2024-11-23 09:44:21 +0530

    Assignment 93

---
+++ TestNG_Prg5.java
@@ -0,0 +1,58 @@
+package testngprograms;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+public class TestNG_Prg5 {
+	@BeforeSuite
+	public void bs()
+	{
+		
+	}
+	@BeforeTest
+	public void b()
+	{
+		
+	}
+	@BeforeClass
+	public void ba()
+	{
+		
+	}
+	@BeforeMethod
+	public void bo()
+	{
+		
+	}
+	@Test
+	public void login()
+	{
+		
+	}
+	@AfterSuite
+	public void Ap()
+	{
+		
+	}
+	@AfterTest
+	public void po()
+	{
+		
+	}
+	@AfterClass
+	public void ui()
+	{
+		
+	}
+	@AfterMethod
+	public void ua()
+	{
+		
+	}
+
+}
